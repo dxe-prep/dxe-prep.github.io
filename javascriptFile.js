e8ea964e818e6a00d42153ab1b72b2af8c91b543
dxe69 <govilshi000@isd284.com>
2019-08-25 23:27:59 -0500

    testing testing

---
+++ javascriptFile.js
@@ -0,0 +1,12 @@
+// alright bro
+
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-125px";
+  }
+  prevScrollpos = currentScrollPos;
+}
